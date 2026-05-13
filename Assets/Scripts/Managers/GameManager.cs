695e0768e23c65a4abab7b4bd4c9d51425bf023f
bfejwndbdu <officiallyrichard@gmail.com>
2026-05-13 16:29:17 +0800

    Reducing light Mechanic

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    public Light visionRadius;
+    public Light lightRadius;
+    private float lightMin;
+
 
     private void Awake()
     {
@@ -18,11 +22,40 @@ public class GameManager : MonoBehaviour
 
         //Persist across scene changes
         DontDestroyOnLoad(gameObject);
-        // visionRadius = GetComponent<Light>();
+        //Initiate values
+        lightMin = 7.0f;
+        //Clamp light radius
+        lightRadius.range = Mathf.Clamp(lightRadius.range,7,20);
+
+        StartCoroutine(ReduceVision(2.0f,1.0f));
     }
-    
+    private IEnumerator ReduceVision(float interval, float amount)
+    {
+        while (true)
+        {
+            if (lightRadius.range > lightMin) //7 is placeholder amount,very low visiblility
+            {
+                lightRadius.range -= amount;
+                if (lightRadius.range < lightMin)
+                {
+                    lightRadius.range = lightMin;
+                }
+                Debug.Log("Light range: " + lightRadius.range);
+                yield return new WaitForSeconds(interval);
+            }
+            else
+            {
+                yield return new WaitForSeconds(interval);
+            }
+        }
+    }
+
     public void ChangeLightRadius(float value)
     {
-        visionRadius.range += value;
+        lightRadius.range += value;
+        if (lightRadius.range > 20f)
+        {
+            lightRadius.range = 20f;
+        }
     }
 }
