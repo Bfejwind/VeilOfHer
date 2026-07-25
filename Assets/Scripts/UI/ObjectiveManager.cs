25c6eedcba7dac1971bc5baa5bdc1bae2cdd0bd6
100hui <S10268881@connect.np.edu.sg>
2026-07-25 23:24:42 +0800

    game start 3 tasks, interaction

--- Assets/Scripts/UI/ObjectiveManager.cs
+++ Assets/Scripts/UI/ObjectiveManager.cs
@@ -1,55 +1,138 @@
+using System.Collections;
 using UnityEngine;
 
 public class ObjectiveManager : MonoBehaviour
 {
+    private enum ObjectiveStage
+    {
+        RepairGenerator,
+        TalkToZyr4,
+        EatBreakfast,
+        Complete
+    }
+
     [Header("UI Systems")]
     [SerializeField] private TaskUIController taskUI;
     [SerializeField] private WaypointUI waypointUI;
 
-    [Header("First Objective")]
-    [SerializeField] private Transform zyr4WaypointTarget;
-    [SerializeField] private string firstTaskText = "Talk to Zyr4";
+    [Header("Waypoint Targets")]
+    [SerializeField] private Transform generatorTarget;
+    [SerializeField] private Transform zyr4Target;
+    [SerializeField] private Transform foodTarget;
 
-    private bool firstObjectiveCompleted;
+    [Header("Interaction Objects")]
+    [SerializeField] private GameObject generatorInteraction;
+    [SerializeField] private GameObject zyr4Interaction;
+    [SerializeField] private GameObject foodInteraction;
+
+    [Header("Timing")]
+    [SerializeField] private float nextTaskDelay = 1.5f;
+
+    private ObjectiveStage currentStage;
 
     private void Start()
     {
-        StartTalkToZyr4Objective();
+        StartGeneratorObjective();
     }
 
-    private void StartTalkToZyr4Objective()
+    private void StartGeneratorObjective()
     {
-        firstObjectiveCompleted = false;
+        currentStage = ObjectiveStage.RepairGenerator;
 
-        if (taskUI != null)
-        {
-            taskUI.ShowTask(firstTaskText);
-        }
+        SetInteractionStates(
+            generatorActive: true,
+            zyr4Active: false,
+            foodActive: false
+        );
 
-        if (waypointUI != null)
-        {
-            waypointUI.SetTarget(zyr4WaypointTarget);
-            waypointUI.ShowWaypoint();
-        }
+        taskUI.ShowTask("Repair the power unit");
+        waypointUI.SetTarget(generatorTarget);
+        waypointUI.ShowWaypoint();
     }
 
-    public void CompleteTalkToZyr4Objective()
+    public void CompleteGeneratorObjective()
     {
-        if (firstObjectiveCompleted)
+        if (currentStage != ObjectiveStage.RepairGenerator)
         {
             return;
         }
 
-        firstObjectiveCompleted = true;
+        generatorInteraction.SetActive(false);
+        waypointUI.HideWaypoint();
+        taskUI.CompleteTask();
 
-        if (waypointUI != null)
+        StartCoroutine(StartZyr4AfterDelay());
+    }
+
+    private IEnumerator StartZyr4AfterDelay()
+    {
+        yield return new WaitForSecondsRealtime(nextTaskDelay);
+
+        currentStage = ObjectiveStage.TalkToZyr4;
+
+        SetInteractionStates(
+            generatorActive: false,
+            zyr4Active: true,
+            foodActive: false
+        );
+
+        taskUI.ShowTask("Return inside and talk to Zyr4");
+        waypointUI.SetTarget(zyr4Target);
+        waypointUI.ShowWaypoint();
+    }
+
+    public void CompleteZyr4Objective()
+    {
+        if (currentStage != ObjectiveStage.TalkToZyr4)
         {
-            waypointUI.HideWaypoint();
+            return;
         }
 
-        if (taskUI != null)
+        zyr4Interaction.SetActive(false);
+        waypointUI.HideWaypoint();
+        taskUI.CompleteTask();
+
+        StartCoroutine(StartBreakfastAfterDelay());
+    }
+
+    private IEnumerator StartBreakfastAfterDelay()
+    {
+        yield return new WaitForSecondsRealtime(nextTaskDelay);
+
+        currentStage = ObjectiveStage.EatBreakfast;
+
+        SetInteractionStates(
+            generatorActive: false,
+            zyr4Active: false,
+            foodActive: true
+        );
+
+        taskUI.ShowTask("Eat breakfast");
+        waypointUI.SetTarget(foodTarget);
+        waypointUI.ShowWaypoint();
+    }
+
+    public void CompleteBreakfastObjective()
+    {
+        if (currentStage != ObjectiveStage.EatBreakfast)
         {
-            taskUI.CompleteTask();
+            return;
         }
+
+        currentStage = ObjectiveStage.Complete;
+
+        foodInteraction.SetActive(false);
+        waypointUI.HideWaypoint();
+        taskUI.CompleteTask();
+    }
+
+    private void SetInteractionStates(
+        bool generatorActive,
+        bool zyr4Active,
+        bool foodActive)
+    {
+        generatorInteraction.SetActive(generatorActive);
+        zyr4Interaction.SetActive(zyr4Active);
+        foodInteraction.SetActive(foodActive);
     }
 }
