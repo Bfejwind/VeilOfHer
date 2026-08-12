bcce948224730603ab23c03ccf478d3c68334220
katriel <s10267336@connect.np.edu.sg>
2026-08-12 22:05:49 +0800

    Added in Day 2 scene and updated the mementos script a bit so that it saves across scenes now

--- Assets/Scripts/Interaction/Mementos.cs
+++ Assets/Scripts/Interaction/Mementos.cs
@@ -5,6 +5,11 @@ using UnityEngine.Events;
 
 public class Mementos : MonoBehaviour, IInteractable
 {
+    [Header("Unique ID")]
+    [Tooltip("Must be unique across ALL mementos in the game")]
+    [SerializeField] 
+    private string mementoID;
+
     [Header("Sparkle Object")]
     [SerializeField]
     public GameObject Sparkle;
@@ -34,6 +39,14 @@ public class Mementos : MonoBehaviour, IInteractable
     void Start()
     {
         input = GetComponent<StarterAssetsInputs>();
+
+        // Check persistent state as soon as this object loads
+        if (MementosManager.Instance != null && MementosManager.Instance.HasCollected(mementoID))
+        {
+            mementoFound = true;
+            gameObject.SetActive(false); // hide the whole object, sparkle, prompt, everything
+            return; // skip the rest of Start(), no point setting anything else up
+        }
     }
 
     public void MementoInventory() // This function is called when the player interacts with the memento object
@@ -63,6 +76,8 @@ public class Mementos : MonoBehaviour, IInteractable
             currentMemento.SetActive(true);
             mementoCanvas.SetActive(true);
             OpenMementoUI();
+
+            MementosManager.Instance.Collect(mementoID); // persist it
             CollectedMemento?.Invoke(); // Invoke the CollectedMemento event to notify other scripts that the memento has been collected
         }
     }
