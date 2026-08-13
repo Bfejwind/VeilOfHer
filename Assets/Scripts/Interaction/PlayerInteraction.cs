3862786b63616dc8217da0e69d3972d34134f22e
katriel <s10267336@connect.np.edu.sg>
2026-08-13 13:03:39 +0800

    Changed the interaction key to F

--- Assets/Scripts/Interaction/PlayerInteraction.cs
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -52,7 +52,7 @@ public class PlayerInteraction : MonoBehaviour
                     hitSomething = true;
                     interactionText.text = interactable.GetDescription();
 
-                    if (Input.GetKeyDown(KeyCode.E))
+                    if (Input.GetKeyDown(KeyCode.F))
                     {
                         interactable.Interact();
                     }
